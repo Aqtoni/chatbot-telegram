c256f6fae6c26d2c2d8280fe98b27c675ec28506
Aqtoni <109670071+Aqtoni@users.noreply.github.com>
2023-05-03 23:02:36 +0300

    UPD: telegram om message

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
+import { Scenes, Telegraf } from 'telegraf';
 
-@Injectable()
-export class TelegramService {}
+type Context = Scenes.SceneContext;
+
+@Update()
+export class TelegramService extends Telegraf<Context> {
+  constructor(private readonly configService: ConfigService) {
+    super(configService.get('TELEGRAM_API'));
+  }
+  @Start()
+  onStart(@Ctx() ctx: Context) {
+    ctx.replyWithHTML(`<b>Привет, ${ctx.from.username}</b>
+This is a chat bot with ChatGPT!
+Enter any phrase and get an answer!
+        `);
+  }
+
+  @On('text')
+  onMessage(@Message('text') message: string) {
+    return `🫠${message}🥳`;
+  }
+}
